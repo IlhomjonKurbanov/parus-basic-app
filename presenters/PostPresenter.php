d960f6fe31f138b4b348e99556c09efd0866c459
rokorolov <rokorolov@gmail.com>
2016-11-07 17:42:41 +0200

    Add presenters

--- presenters/PostPresenter.php
+++ presenters/PostPresenter.php
@@ -3,7 +3,6 @@
 namespace app\presenters;
 
 use rokorolov\parus\admin\base\BasePresenter;
-use rokorolov\helpers\Html;
 use Yii;
 
 /**
@@ -13,21 +12,46 @@ use Yii;
  */
 class PostPresenter extends BasePresenter
 {
-    public function published_at_date()
+    const IMAGE_EXTENSION = 'jpg';
+    
+    public function published_at()
     {
         return Yii::$app->formatter->asDate($this->wrappedObject->published_at);
     }
     
-    public function published_at_date_long()
+    public function published_at_relative()
     {
-        return Yii::$app->formatter->asDate($this->wrappedObject->published_at, 'long');
+        return Yii::$app->formatter->asRelativeTime($this->wrappedObject->published_at);
     }
     
     public function image_large_src()
     {
-        if (!empty($this->wrappedObject->image)) {
-            return Yii::getAlias('@web/uploads/post/' . $this->wrappedObject->id . '/' . $this->wrappedObject->image . '-large.jpg');
+        return $this->full_image_src('large');
+    }
+    
+    public function image_small_src()
+    {
+        return $this->full_image_src('small');
+    }
+    
+    public function image_thumb_src()
+    {
+        return $this->full_image_src('thumb');
+    }
+    
+    public function full_image_src($prefix = null)
+    {
+        if (!$this->existsImage()) {
+            return null;
         }
-        return null;
+        
+        $prefix = $prefix ? '-' . $prefix : '';
+        
+        return Yii::getAlias('@web/uploads/post/' . $this->wrappedObject->id . '/' . $this->wrappedObject->image . $prefix . '.' . self::IMAGE_EXTENSION);
+    }
+    
+    public function existsImage()
+    {
+        return !empty($this->wrappedObject->image);
     }
 }
