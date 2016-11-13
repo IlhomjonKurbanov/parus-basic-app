cabeb3e7928498f9bc5ef65ef4dd1c5901122fc1
rokorolov <rokorolov@gmail.com>
2016-11-13 18:50:45 +0200

    Update styles

--- views/page/view.php
+++ views/page/view.php
@@ -5,11 +5,10 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 
 $this->title = $page->title;
-$this->params['breadcrumbs'] = [$this->title];
 $this->registerMetaTag(['name' => 'description', 'content' => $page->meta_description], 'description');
 
 ?>
-<div class="page-view">
+<div class="page page-view">
     <h1><?= Html::encode($page->title) ?></h1>
     <?= $page->content ?>
 </div>
