a5f9c6e64ad13ddfc084df11f52bcc9f5fc44137
roxblnfk <roxblnfk@mail.ru>
2016-11-24 13:51:30 +0300

    config files changed

--- config/db.php
+++ config/db.php
@@ -1,9 +1,12 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
+return array_merge(
+    [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ],
+    is_file(__DIR__ . DIRECTORY_SEPARATOR . 'db-local.php') ? include_once __DIR__ . DIRECTORY_SEPARATOR . 'db-local.php' : []
+);
